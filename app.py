5a30f21f41d5e73207b030397e8558f9748bc453
Aleida Rodriguez <93956966+Aleidarodri@users.noreply.github.com>
2023-03-08 19:22:48 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+# python3 -m venv venv
+# source virtual/bin/active
+# flask --app
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "hola Ite"
+
+
+@app.route("/bye")
+def adios():
+    return "bye bye"
+
+
+if __name__ == '__main__':
+    app.run()
